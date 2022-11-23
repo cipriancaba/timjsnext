8023bbbdd877e518ab66e536f736a3f67c7fa367
Ciprian Caba <caba.ciprian@gmail.com>
2022-11-23 16:24:34 +0200

    Timer

--- components/UIHeader.tsx
+++ components/UIHeader.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link"
 
 export const UIHeader = () => {
+  const date = new Date()
   return (
     <header className="border-b p-4">
       <nav className="flex gap-4">
         <Link href="/">Home</Link>
         <Link href="/cats">Show me the cats</Link>
         <Link href="/checkout">Buy a cat</Link>
+        <div className="flex-1"></div>
+        <span>{date.toUTCString()}</span>
       </nav>
     </header>
   )
